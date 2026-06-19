169721c10d0a49c9e6b529f670e054f2367a7eb3
ahmedabdelhalemmohamed <ahmedabdelhalemmohamed2026@gmail.com>
2026-06-19 19:25:41 +0300

    trying with supabase and handle with input and insert data to supabase and disply in browser

--- src/App.tsx
+++ src/App.tsx
@@ -38,10 +38,45 @@ function App() {
   const [profiles, setProfiles] = useState<Profile[]>([])
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState<string | null>(null)
+  const [input, setInput] = useState<string>('')
+
+  console.log(profiles);
+
+  const sendToSupabase = async () => {
+    const {data, error} = await supabase.from("profiles").insert({
+      name: input
+    }).select();
+
+    if(error) {
+      setError(error.message);
+      return;
+    }
+
+    if(data && data.length > 0) {
+      console.log(data);
+      const newProfile: Profile = {
+        id: data[0].id,
+        name: data[0].name,
+        created_at: new Date(data[0].created_at)
+      };
+
+      setProfiles((prev)=> [...prev, newProfile]);
+      setInput('');
+    }
+
+  }
+  
+
+
+  useEffect(() => {
+  // 1. نضع متغيراً يشير إلى أن المكون نشط حالياً
+  let active = true;
 
   async function fetchProfiles() {
-    setLoading(true)
     const { data, error } = await supabase.from("profiles").select()
+
+    // 2. قبل تحديث الـ State، نتأكد أن المكون ما زال نشطاً ولم يغلقه المستخدم
+    if (!active) return;
 
     console.log(data);
     if (error) {
@@ -54,9 +89,13 @@ function App() {
     setLoading(false)
   }
 
-  useEffect(() => {
-    fetchProfiles()
-  }, [])
+  fetchProfiles()
+
+  // 3. دالة التنظيف (Cleanup function) تشتغل إذا غادر المستخدم الصفحة
+  return () => {
+    active = false;
+  };
+}, []) // المصفوفة فارغة ليعمل مرة واحدة فقط عند فتح الصفحة
 
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error: {error}</p>
@@ -66,6 +105,9 @@ function App() {
       {profiles.map(profile => (
         <div key={profile.id}>{profile.name}</div>
       ))}
+
+      <input type="text" value={input} onChange={(e) => setInput(e.target.value)} className="border-2 border-red-500"/>
+      <button onClick={sendToSupabase}>Add</button>
     </>
   )
 }
